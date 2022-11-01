fb0f6858c09ff9579738a18b1f79529daa2e9404
Gustavo Augusto Wustemberg <gustavowustemberg14@gmail.com>
2022-11-01 12:41:46 -0300

    force code

--- treatImg.py
+++ treatImg.py
@@ -7,7 +7,6 @@ x = 0
 def calc_new_height(width, hegth, new_width):
     return round(new_width * hegth / width)
 
-
 def is_image(extension):
     extension_lowercase = extension.lower()
     return bool(re.search(r'^\.(jpe?g|png|webp)$', extension_lowercase))
