a2ad0fb11f9ddd1b43638f35709f292acc3e2a6d
Gustavo Augusto Wustemberg <97157543+GustavoWustemberg@users.noreply.github.com>
2023-08-23 17:05:29 -0300

    [FEAT] added menu selection

--- Class/initializer.py
+++ Class/initializer.py
@@ -2,8 +2,11 @@ from Functions.converter import converter
 from Functions.resize import resize
 from Functions.compact import compact
 from Functions.removeBG import removeBG
+import questionary
 
+optionsConvertion = ['jpg', 'jpeg', 'png', 'webp']
 class initializer:
+
     def __init__(self, root_folder, new_width, value_quality, type_image):
         self.root_folder = root_folder
         self.new_width = new_width
@@ -18,8 +21,12 @@ class initializer:
             self.value_quality = int(input('Digite o valor entre 0 a 100 da porcentagem de perca de qualidade desejada: '))
             compact(self.root_folder, self.value_quality)
         elif x == 3:
-            self.type_image = input('Tipos de Imagens: JPG, JPEG, PNG, WEBP\n'
-                       'Digite um dos tipos citados acima para converter as imagens: ').lower()
+            selected_option = questionary.select(
+                'Selecione uma das opções abaixo:',
+                choices=optionsConvertion
+            ).ask()
+
+            self.type_image = selected_option
             converter(self.root_folder, self.type_image)
         elif x == 4:
             removeBG(self.root_folder)
